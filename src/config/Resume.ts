60386faf05b5148c1f04beabbfa3884da4c32252
Amaan Patel <amaanp32@gmail.com>
2026-05-01 00:58:39 +0530

    change resume

--- src/config/Resume.ts
+++ src/config/Resume.ts
@@ -1,3 +1,5 @@
 export const resumeConfig = {
-  url: 'https://drive.google.com/file/d/1sODK6tHwQq27q86kyb1bhGhe4U4UlB_M/preview',
+  // url: 'https://drive.google.com/file/d/1sODK6tHwQq27q86kyb1bhGhe4U4UlB_M/preview',
+  url: 'https://drive.google.com/file/d/1MVXX_Zh12XimNtBcN9t3FY9gK1bt0XQM/preview',
+  
 };
